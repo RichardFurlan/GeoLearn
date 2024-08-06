4909891787dfcc2af7f50f02f7bbbf6b1f783456
Richard Figueiredo Furlan <emsrichard10@hotmail.com>
2024-08-05 23:18:51 -0300

    Adicionando metodos de Get e Update dentro do userRepository

--- GeoLearn.Domain/Repositories/IUserRepository.cs
+++ GeoLearn.Domain/Repositories/IUserRepository.cs
@@ -4,5 +4,7 @@ namespace GeoLearn.Domain.Repositories;
 
 public interface IUserRepository
 {
-    Task<User> GetUserByEmailAndPasswordAsync(string email, string passwordHash);
+    Task<User?> GetUserByEmailAndPasswordAsync(string email, string passwordHash);
+    Task<User?> GetByIdAsync(int id);
+    Task UpdateAsync(User user);
 }
